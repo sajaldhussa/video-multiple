c4e5df0f19aee04f9d9a05105ec606daf7c6cfaf
Dhussa <sdhussa@apac.corpdir.net>
2020-06-03 23:49:01 +0800

    Fix

--- server/app.js
+++ server/app.js
@@ -11,10 +11,10 @@ const MeetingRoutes = require('./routes/meeting')
 app.use('/meeting', MeetingRoutes)
 
 app.set('socketio', io);
-
+const meetingId;;
 io.on('connection', function(socket){
    socket.on('create', function(room) {
-      
+	  meetingId = room;
       socket.join(room);
       io.sockets.in(room).emit("user-joined", socket.id, io.nsps['/'].adapter.rooms[room].length, Object.keys(io.nsps['/'].adapter.rooms[room].sockets), room);
   });
@@ -26,11 +26,11 @@ io.on('connection', function(socket){
   	});
 
     socket.on("message", function(data){
-		io.sockets.emit("broadcast-message", socket.id, data);
+		io.sockets.in(meetingId).emit("broadcast-message", socket.id, data);
     })
 
 	socket.on('disconnect', function() {
-		io.sockets.emit("user-left", socket.id);
+		io.sockets.in(meetingId).emit("user-left", socket.id);
 	})
 });
 
